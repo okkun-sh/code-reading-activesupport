b49d20d06aca2e8cbc223e5436ca62aeacf2b5bb
okkun_sh <okkun.sh@gmail.com>
2019-02-04 00:09:16 +0900

    added String#remove String#squish

---
+++ string/remove.rb
@@ -0,0 +1,9 @@
+require_relative "../base"
+
+# remove
+binding.pry
+str = "string"
+new_str = str.remove("i")
+
+puts str
+puts new_str
---
+++ string/squish.rb
@@ -0,0 +1,9 @@
+require_relative "../base"
+
+# squish
+binding.pry
+str = "\sstr\s\s\sing\s"
+new_str = str.squish
+
+puts str
+puts new_str
